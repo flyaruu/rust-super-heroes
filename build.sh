b1e3adc68716a23da35ff8ce38cb9365130e1b30
Frank Lyaruu <flyaruu@gmail.com>
2025-04-20 13:46:54 +0200

    full run, clean up a bit

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker build . -t superhero/grpc-locations:latest --target grpc-locations
-docker build . -t superhero/rest-heroes:latest --target rest-heroes
-docker build . -t superhero/rest-villains:latest --target rest-villains
-docker build . -t superhero/rest-fights:latest --target rest-fights
